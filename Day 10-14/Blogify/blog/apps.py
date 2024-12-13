f9b0fd98c629fbff14bd62c77d3be660fb25030f
rohan-tridhyatech <rohanmovaliya.tridhyatech@gmail.com>
2024-12-13 17:55:46 +0530

    Delete Files

--- Day 10-14/Blogify/blog/apps.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-
-from django.apps import AppConfig
-
-# Configuration class for the Blog application
-class BlogConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"  # Default primary key field type
-    name = "blog"  # Name of the application
